0104870a9673170ae3ea2e4bdb709d0a52b6016f
John Kariuki <john.kariuki@andela.com>
2016-09-26 23:23:11 +0300

    (chore:tests) Add frontend service tests

---
+++ test/frontend/services/toast-test.js
@@ -0,0 +1,20 @@
+describe('Toast service', function () {
+  var Toast, $mdToast;
+
+  //Load the mock application module.
+  beforeEach(module('Bookmark'));
+
+  //Inject the Toast and $httpBackend services to our tests.
+  beforeEach(inject(function (_Toast_, _$mdToast_) {
+    Toast = _Toast_;
+    $mdToast = _$mdToast_;
+    spyOn($mdToast, 'show');
+  }));
+
+  describe('Toast tests', function () {
+    it('show call $mdToast.show method', function () {
+      Toast.show('message', 'top right', 1000);
+      expect($mdToast.show).toHaveBeenCalled();
+    });
+  });
+});
